3502d04a471a9b9c1ed23852b5e56ff903ac7b21
Shauna Empringham <shaunathedead86@gmail.com>
2022-03-03 11:40:15 -0500

    adding a Promise.all function to useEffect to grab data from api and then setState using the data

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,64 +4,37 @@ import axios from 'axios'
 import 'components/Application.scss'
 import DayList from 'components/DayList'
 import Appointment from './Appointment'
+import { getAppointmentsForDay } from 'helpers/selectors'
 
 export default function Application (props) {
   const [state, setState] = useState({
     day: 'Monday',
-    days: []
-		// appointments: {}
+    days: [],
+    appointments: {},
+    interviewers: {}
   })
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day)
+
   const setDay = day => setState({ ...state, day })
-  const setDays = days => setState(prev => ({ ...prev, days }))
+	// const setDays = days => setState(prev => ({ ...prev, days }))
 
   useEffect(() => {
-    axios
-			.get('/api/days')
-			.then(res => setDays(res.data))
-			.catch(err => console.log(err))
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then(all => {
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }))
+    })
   }, [])
 
-  const appointments = {
-    '1': {
-      id: 1,
-      time: '12pm'
-    },
-    '2': {
-      id: 2,
-      time: '1pm',
-      interview: {
-        student: 'Lydia Miller-Jones',
-        interviewer: {
-          id: 3,
-          name: 'Sylvia Palmer',
-          avatar: 'https://i.imgur.com/LpaY82x.png'
-        }
-      }
-    },
-    '3': {
-      id: 3,
-      time: '2pm'
-    },
-    '4': {
-      id: 4,
-      time: '3pm',
-      interview: {
-        student: 'Archie Andrews',
-        interviewer: {
-          id: 4,
-          name: 'Cohana Roy',
-          avatar: 'https://i.imgur.com/FK8V841.jpg'
-        }
-      }
-    },
-    '5': {
-      id: 5,
-      time: '4pm'
-    }
-  }
-
-  const parsedAppointments = Object.values(appointments).map(appointment =>
+  const parsedAppointments = dailyAppointments.map(appointment =>
     <Appointment key={appointment.id} {...appointment} />
 	)
 
