fe62c24dd451d6194b237a687c8ce8562c0a671f
Shauna Empringham <shaunathedead86@gmail.com>
2022-03-03 10:04:53 -0500

    adding form functionality and stories

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from 'components/Appointment/Show'
 import Confirm from 'components/Appointment/Confirm'
 import Status from 'components/Appointment/Status'
 import Error from 'components/Appointment/Error'
+import Form from 'components/Appointment/Form'
 
 storiesOf('Button', module)
 	.addParameters({
@@ -171,5 +172,21 @@ storiesOf('Appointment', module)
   <Error
     message='Could not delete the appointment'
     onClose={action('onClose')}
+		/>
+	)
+	.add('Edit', () =>
+  <Form
+    student='Shauna'
+    interviewer={1}
+    interviewers={interviewers}
+    onSave={action('onSave')}
+    onCancel={action('onCancel')}
+		/>
+	)
+	.add('Create', () =>
+  <Form
+    interviewers={interviewers}
+    onSave={action('onSave')}
+    onCancel={action('onCancel')}
 		/>
 	)
